b3b16f5ab2bda12adb7fe49391e160beec1e2bb5
ezioleq <git@ezioleq.com>
2024-08-19 14:25:38 +0200

    Add background glow and noise
    Btw I forgor to import the navbar component previously

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Canvas } from '@threlte/core';
   import Scene from './lib/Scene.svelte';
+  import Navbar from './lib/Navbar.svelte';
 </script>
 
 <main class="container">
@@ -14,5 +15,27 @@
 <style>
   .container {
     height: 100vh;
+  }
+
+  .background-glow {
+    position: absolute;
+    z-index: -100;
+
+    width: 0px;
+    height: 0px;
+    
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    
+    background-color: rgba(138, 43, 226, 0.9);
+    box-shadow: 0 0 50vh 20vh rgba(138, 43, 226, 0.65);
+  }
+
+  @media (prefers-color-scheme: light) {
+    .background-glow {
+      background-color: rgba(138, 43, 226, 0.9);
+      box-shadow: 0 0 50vh 20vh rgba(138, 43, 226, 0.65);
+    }
   }
 </style>
