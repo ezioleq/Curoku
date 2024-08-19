d23283fc5807af7f69e9307563cd0557f734f82c
ezioleq <git@ezioleq.com>
2024-08-19 14:23:12 +0200

    Tweak orbit camera settings

--- client/src/lib/Scene.svelte
+++ client/src/lib/Scene.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
   import { extend, T, useTask, useThrelte } from '@threlte/core';
-  import { type Mesh } from 'three';
+  import { MOUSE, type Mesh } from 'three';
   import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
   extend({OrbitControls})  
 
   let cube: Mesh;
   
-  const {camera, renderer} = useThrelte();
-  camera.current.position.z = 1.5
+  const { camera, renderer } = useThrelte();  
 
   useTask((delta) => {})
 </script>
@@ -19,5 +18,20 @@
     <T.MeshBasicMaterial color="white"/>
   </T.Mesh>
 
-  <T.OrbitControls args={[$camera, renderer.domElement]}/>
+  <T.PerspectiveCamera
+    makeDefault
+    near={0.01}
+    position.z={3}
+  >
+    <T.OrbitControls args={[$camera, renderer.domElement]}
+      minDistance={0.5}
+      maxDistance={4}
+      zoomToCursor={true}
+      mouseButtons={{
+        LEFT: null,
+        MIDDLE: MOUSE.PAN,
+        RIGHT: MOUSE.ROTATE
+      }}
+    />
+  </T.PerspectiveCamera>
 </group>
